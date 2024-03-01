db3a20b7e1eb5e929b87b80325b2039bef7b7d0b
Ubuntu <ubuntu@ip-172-31-36-134.ec2.internal>
2024-03-01 02:48:43 +0000

    Update dolma indexing commands

--- train/index_dolma.sh
+++ train/index_dolma.sh
@@ -5,3 +5,18 @@ for subset in "${subsets[@]}"; do
     echo ${subset}
     python main.py --data_dir /data_c/dolma-v1_6/${subset} --temp_dir /data_t/v4_dolma-v1_6-${subset}_llama --save_dir /data_i/v4_dolma-v1_6-${subset}_llama --tokenizer llama --cpus 128 --mem 850
 done
+
+
+subsets=("head" "middle" "tail")
+
+for subset in "${subsets[@]}"; do
+    if [ "$subset" = "tail" ]; then
+        shards=4
+    else
+        shards=2
+    fi
+    echo ""
+    echo "cc_${subset}"
+    echo "${shards}"
+    python main.py --data_dir /data_c/dolma-v1_6/cc_en_${subset} --temp_dir /data_t/v4_dolma-v1_6-cc_en_${subset}_llama --save_dir /data_i_${subset}/v4_dolma-v1_6-cc_en_${subset}_llama --tokenizer llama --shards ${shards} --cpus 128 --mem 850
+done
