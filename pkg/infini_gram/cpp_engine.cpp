16a05ff33c880c97329e68c6598854e6440b79c7
Jiacheng Liu <liujch1998@gmail.com>
2024-10-24 21:29:29 +0000

    Consolidate changes in the infini-gram-playground repo into here

--- pkg/infini_gram/cpp_engine.cpp
+++ pkg/infini_gram/cpp_engine.cpp
@@ -61,8 +61,43 @@ PYBIND11_MODULE(cpp_engine, m) {
         .def_readwrite("idxs", &SearchDocsResult::idxs)
         .def_readwrite("docs", &SearchDocsResult::docs);
 
+    py::class_<CreativityResult>(m, "CreativityResult")
+        .def_readwrite("rs", &CreativityResult::rs);
+
+    py::class_<AttributionDoc>(m, "AttributionDoc")
+        .def_readwrite("s", &AttributionDoc::s)
+        .def_readwrite("ptr", &AttributionDoc::ptr);
+
+    py::class_<AttributionSpan>(m, "AttributionSpan")
+        .def_readwrite("l", &AttributionSpan::l)
+        .def_readwrite("r", &AttributionSpan::r)
+        .def_readwrite("length", &AttributionSpan::length)
+        .def_readwrite("count", &AttributionSpan::count)
+        .def_readwrite("docs", &AttributionSpan::docs);
+
+    py::class_<AttributionResult>(m, "AttributionResult")
+        .def_readwrite("spans", &AttributionResult::spans);
+
+    py::class_<Attribution2Doc>(m, "Attribution2Doc")
+        .def_readwrite("s", &Attribution2Doc::s)
+        .def_readwrite("local_doc_ix", &Attribution2Doc::local_doc_ix)
+        .def_readwrite("doc_start_ptr", &Attribution2Doc::doc_start_ptr)
+        .def_readwrite("doc_end_ptr", &Attribution2Doc::doc_end_ptr)
+        .def_readwrite("doc_ix", &Attribution2Doc::doc_ix)
+        .def_readwrite("doc_len", &Attribution2Doc::doc_len)
+        .def_readwrite("disp_len", &Attribution2Doc::disp_len)
+        .def_readwrite("disp_offset", &Attribution2Doc::disp_offset)
+        .def_readwrite("metadata", &Attribution2Doc::metadata)
+        .def_readwrite("token_ids", &Attribution2Doc::token_ids)
+        .def_readwrite("token_offset_span_pairs", &Attribution2Doc::token_offset_span_pairs)
+        .def_readwrite("total_matched_len", &Attribution2Doc::total_matched_len);
+
+    py::class_<Attribution2Result>(m, "Attribution2Result")
+        .def_readwrite("spans", &Attribution2Result::spans)
+        .def_readwrite("docs", &Attribution2Result::docs);
+
     py::class_<Engine>(m, "Engine")
-        .def(py::init<const vector<string>, const U16, const bool, const size_t, const size_t, const size_t>())
+        .def(py::init<const vector<string>, const U16, const bool, const size_t, const size_t, const size_t, const set<U16>>())
         .def("find", &Engine::find, "input_ids"_a)
         .def("find_cnf", &Engine::find_cnf, "cnf"_a, "max_clause_freq"_a, "max_diff_tokens"_a)
         .def("count", &Engine::count, "input_ids"_a)
@@ -74,10 +109,16 @@ PYBIND11_MODULE(cpp_engine, m) {
         .def("search_docs", &Engine::search_docs, "input_ids"_a, "maxnum"_a, "max_disp_len"_a)
         .def("search_docs_cnf", &Engine::search_docs_cnf, "cnf"_a, "maxnum"_a, "max_disp_len"_a, "max_clause_freq"_a, "max_diff_tokens"_a)
         .def("get_doc_by_rank", &Engine::get_doc_by_rank, "s"_a, "rank"_a, "max_disp_len"_a)
+        .def("get_docs_by_ranks", &Engine::get_docs_by_ranks, "list_of_s_and_rank"_a, "max_disp_len"_a)
         .def("get_doc_by_ptr", &Engine::get_doc_by_ptr, "s"_a, "ptr"_a, "max_disp_len"_a)
+        .def("get_docs_by_ptrs", &Engine::get_docs_by_ptrs, "list_of_s_and_ptr"_a, "max_disp_len"_a)
         .def("get_doc_by_ix", &Engine::get_doc_by_ix, "doc_ix"_a, "max_disp_len"_a)
+        .def("get_docs_by_ixs", &Engine::get_docs_by_ixs, "list_of_doc_ix"_a, "max_disp_len"_a)
         .def("get_num_shards", &Engine::get_num_shards)
         .def("get_tok_cnt", &Engine::get_tok_cnt, "s"_a)
         .def("get_ds_size", &Engine::get_ds_size, "s"_a)
-        .def("get_total_doc_cnt", &Engine::get_total_doc_cnt);
+        .def("get_total_doc_cnt", &Engine::get_total_doc_cnt)
+        .def("creativity", &Engine::creativity, "input_ids"_a)
+        .def("attribute", &Engine::attribute, "input_ids"_a, "delim_ids"_a, "min_len"_a, "max_cnt"_a, "enforce_bow"_a)
+        .def("attribute_2", &Engine::attribute_2, "input_ids"_a, "delim_ids"_a, "min_len"_a, "max_cnt"_a, "max_docs"_a, "max_disp_len"_a);
 }
