78bd1bfc145094a93706569e6b83809da45d3b27
Jiacheng Liu <liujch1998@gmail.com>
2025-02-21 07:41:19 +0000

    Remove old train and inference files; Move scripts

--- inference/remount.sh
+++
@@ -1,6 +0,0 @@
-sudo mount /dev/nvme2n1 /data-v4-dolma-v1_6-sample-llama
-sudo mount /dev/nvme3n1 /data-v4-dolma-v1_6-cc_en_tail-llama
-sudo mount /dev/nvme4n1 /data-v4-dolma-v1_6-cc_en_middle-llama
-sudo mount /dev/nvme5n1 /data-v4-dolma-v1_6-cc_en_head-llama
-sudo mount /dev/nvme0n1 /data-v4-dolma-v1_6-nocc-llama
-
